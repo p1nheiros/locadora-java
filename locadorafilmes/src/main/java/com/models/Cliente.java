29aa89862435ee6434354d752dbba26bdb010d8d
p1nheiros <pinheiros.dev@gmail.com>
2024-03-21 03:40:26 -0300

    subindo projeto em fase de teste tentando compilar o projeto.

---
+++ locadorafilmes/src/main/java/com/models/Cliente.java
@@ -0,0 +1,70 @@
+package com.models;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nome;
+    private String telefone;
+    private String email;
+    private String endereco;
+    private String cpf;
+    private Date dataCliente;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public Date getDataCliente() {
+        return dataCliente;
+    }
+    public void setDataCliente(Date dataCliente) {
+        this.dataCliente = dataCliente;
+    }
+
+    
+}
